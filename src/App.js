abdf676bf2824187805097de5e8310b3943e1350
Your Name <you@example.com>
2021-07-26 21:24:18 +0530

    use reducer for the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
+import {Container} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello World</h1>
-    </div>
-  );
+
+import {TodoContext} from "./context/TodoContext";
+import todoreducer from "./context/reducer"
+import { useReducer } from "react";
+
+const App =()=>{
+  const [todos,dispatch]=useReducer(todoreducer,[])
+  return(
+    <TodoContext.Provider value={{todos,dispatch}}>
+      <h1>To do App with Context API</h1>
+
+    </TodoContext.Provider>
+  
+  )
 }
 
 export default App;
