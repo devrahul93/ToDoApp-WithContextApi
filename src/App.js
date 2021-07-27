32839f3b2e2c0290a627f33e0e93b55258e4c50a
Your Name <you@example.com>
2021-07-27 15:15:59 +0530

    sending a dispatch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import {Container} from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import "./App.css";
 
 
 import {TodoContext} from "./context/TodoContext";
 import todoreducer from "./context/reducer"
-import { useReducer } from "react";
+import {useReducer} from "react";
 import TodoForm from "./Components/ToDoForm";
 
 const App =()=>{
-  const [todos,dispatch]=useReducer(todoreducer,[])
+  const [todos,dispatch]=useReducer( todoreducer,[] )
   return(
     <TodoContext.Provider value={{todos,dispatch}}>
       <Container fluid>
       <h1>To do App with Context API</h1>
       <TodoForm/>
       </Container>
+      
     </TodoContext.Provider>
   
   )
--- src/Components/ToDoForm.js
+++ src/Components/ToDoForm.js
@@ -1,39 +1,63 @@
-import {useState,useContext} from "react";
+import React, { useState, useContext } from "react";
 import {
-    Form,
-    FormGroup,
-    Input,
-    InputGroup,
-    Button,
-    InputGroupAddon
-} from "reactstrap"
+  Form,
+  FormGroup,
+  Input,
+  Button,
+  InputGroup,
+  InputGroupAddon,
+} from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import '../App.css';
-import {v4} from "uuid"
-import {TodoContext} from "../context/TodoContext"
-import {ADD_TODO} from "../context/action.types"
+import "../App.css";
 
-const TodoForm = ()=>{
-    return(
-        <Form>
-            <FormGroup>
-                <InputGroup>
-                <Input
-                type='text'
-                name='todo'
-                id='todo'
-                placeholder='Next To Do'
-                //haven't added value and onchange
-                />
-                <InputGroupAddon addonType='prepend'>
-                   <Button color='warning'></Button>
-                </InputGroupAddon>
-                
-                </InputGroup>
-                
-            </FormGroup>
-        </Form>
-    )
-}
+import { v4 } from "uuid";
+import { TodoContext } from "../context/TodoContext";
+import { ADD_TODO } from "../context/action.types";
 
-export default TodoForm
+const TodoForm = () => {
+    const [todoString,setTodoString] = useState("");
+    const {dispatch} = useContext(TodoContext);
+
+    //event handling
+    const handleSubmit = e => {
+        e.preventDefault();
+        if(todoString === ""){
+            return alert('enter a todo')
+        }
+        const todo = {
+            todoString,
+            id:v4()
+        }
+        dispatch({
+            type: ADD_TODO,
+            payload: todo
+        })
+        setTodoString("")
+    }
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <InputGroup>
+          <Input
+            type="text"
+            name="todo"
+            id="todo"
+            placeholder="Your next Todo"
+            value={todoString}
+            onChange={e=>setTodoString(e.target.value)}
+          />
+          <InputGroupAddon addonType="prepend">
+            <Button
+              color="warning"
+              //TODO: onclick
+            >
+              Add
+            </Button>
+          </InputGroupAddon>
+        </InputGroup>
+      </FormGroup>
+    </Form>
+  );
+};
+
+export default TodoForm;
